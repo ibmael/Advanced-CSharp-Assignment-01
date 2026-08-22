347a4e0e3f9e6b4b60cba6c3e9a030702f0c01c9
ibrahim mahmoud elghandour <ibrahimelghandour51@gmail.com>
2026-08-22 13:53:49 +0300

    Add project files.

---
+++ AdvancedCSharp01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdvancedCSharp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
